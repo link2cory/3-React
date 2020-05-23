4927d6c7052542a68be1274124ac8df1d522c72b
Cory Perkins <link2cory@gmail.com>
2020-05-23 11:11:36 -0700

    added student registration method

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -12,4 +12,15 @@ class Bootcamp {
     this.level = level;
     this.students = students;
   }
+
+  registerStudent(student) {
+    // check if this student already exists
+    if (
+      this.students.filter(
+        elem => (elem.email == student.email)
+      ).length === 0
+    ) {
+      this.students.push(student)
+    }
+  }
 }
