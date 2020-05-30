37ee968d06cb852db85725abb6f0b4cb8f9cb856
Cory Perkins <link2cory@gmail.com>
2020-05-30 10:43:31 -0700

    completed Task 2: transferred the campsite detail rendering logic to the CampsiteInfo Component

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap'
 
 class CampsiteInfo extends Component {
   constructor(props) {
@@ -6,9 +7,27 @@ class CampsiteInfo extends Component {
     this.state = {};
   }
 
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    )
+  }
+
   render() {
     return this.props.campsite ?
-      <div className="row" />
+      (
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+        </div>
+      )
       : <div />;
   }
 }
