1394c65b2c5f00ab0ee36e5955e3bb78d32fd29f
Cory Perkins <link2cory@gmail.com>
2020-06-05 14:31:38 -0700

    --amend

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-function RenderDirectoryItem({ campsite, onClick }) {
+function RenderDirectoryItem({ campsite }) {
   return (
     <div className="col-md-5 m-1" key={campsite.id}>
-      <Card onClick={() => onClick(campsite.id)}>
+      <Card>
         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
         <CardImgOverlay>
           <CardTitle>{campsite.name}</CardTitle>
@@ -16,7 +16,7 @@ function RenderDirectoryItem({ campsite, onClick }) {
 
 function Directory(props) {
   const directory = props.campsites.map((campsite) => (
-    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+    <RenderDirectoryItem campsite={campsite} />
   ));
   return (
     <div className="container">
