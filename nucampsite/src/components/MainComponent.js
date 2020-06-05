a805c869d252fc985a1e705271cc565907375f24
Cory Perkins <link2cory@gmail.com>
2020-06-05 15:27:04 -0700

    added promotions and partners content, and now home page displays a featured one of each

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
@@ -7,17 +6,35 @@ import Contact from "./ContactComponent";
 import Directory from "./DirectoryComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import { CAMPSITES } from "../shared/campsites";
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
+
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          campsite={
+            this.state.campsites.filter((campsite) => campsite.featured)[0]
+          }
+          partner={this.state.partners.filter((partner) => partner.featured)[0]}
+          promotion={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+        />
+      );
     };
     return (
       <div className="App">
