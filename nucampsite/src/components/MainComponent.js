39ad0a09ee769ca78e691e760ff0daeb417a8181
Cory Perkins <link2cory@gmail.com>
2020-06-05 13:55:24 -0700

    Nucamp Week 3: React Router Excercise 2;  Added FooterComponent

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
 import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Main extends Component {
@@ -32,6 +33,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer />
       </div>
     );
   }
